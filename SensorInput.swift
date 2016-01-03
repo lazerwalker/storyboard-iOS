c3ee79ef5430e5ff2cc89342cb90903bff7966c0
Mike Walker <michael@lazerwalker.com>
2016-01-02 20:54:14 -0800

    Add comments on SensorInput protocol

--- SensorInput.swift
+++ SensorInput.swift
@@ -1,5 +1,11 @@
 import Foundation
 
 protocol SensorInput {
+    /* As a SensorInput, you should store this callback
+     * and call it whenever your data changes.
+     *
+     * By convention, your implementation of onChange should immediately call
+     * the passed callback with the current/initial value of your sensor.
+     */
     func onChange(cb:(value:AnyObject) -> Void)
 }
